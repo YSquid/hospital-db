901f03d30f037740e9b41c19b356f8de31e7d34c
Ysquid <ahk648@gmail.com>
2022-12-12 11:42:42 -0600

    initial commit

---
+++ hospital_db.sql
@@ -0,0 +1,15 @@
+CREATE TABLE doctors (
+    id SERIAL PRIMARY KEY
+    name_first VARCHAR(32)
+    name_last VARCHAR(32)
+)
+
+CREATE TABLE patients (
+    id SERIAL PRIMARY KEY
+    name_first VARCHAR(32)
+    name_last VARCHAR(32)
+)
+
+CREATE TABLE beds (
+    
+)
